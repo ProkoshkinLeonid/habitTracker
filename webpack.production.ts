27f63907aa47d2716e5679e9daaa24ccecfc216d
prokoshkin@cmsplanet.ru <JfTtv42m>
2022-07-01 20:03:52 +0300

    renamed

--- webpack.production.ts
+++ webpack.production.ts
@@ -1,19 +1,19 @@
-// const merge = require('webpack-merge');
-//
-// const common = require('./webpack.common.ts');
-//
-// module.exports = merge(common, {
-//     mode: 'production',
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.ts$/,
-//                 enforce: 'pre',
-//                 loader: 'ts-loader',
-//                 options: {
-//                     configFile: 'tsconfig.prod.json'
-//                 }
-//             }
-//         ],
-//     }
-// });
+const merge = require('webpack-merge');
+
+const common = require('./webpack.config.ts');
+
+module.exports = merge(common, {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'ts-loader',
+                options: {
+                    configFile: 'tsconfig.prod.json'
+                }
+            }
+        ],
+    }
+});
