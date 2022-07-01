8c9913ba949df136ba71f6d39f26f209a419b68b
prokoshkin@cmsplanet.ru <JfTtv42m>
2022-07-01 20:57:42 +0300

    fix configs for building project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react'
+import {Main} from "./pages/main/Main";
 
 
 export const App: FC = () => {
-    return <div>Hello World!!!</div>
+    return <div>Hello World!!!<Main/></div>
 }
---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Main = () => {
+    return <h1>Its main page</h1>
+}
